c332d9d3affbcc63bb91d4c64b09de247db8a1e1
Jesse Stricker <git@jessestricker.de>
2025-12-29 19:15:11 +0100

    feat: setup basic logging and cli

--- src/kontor/__init__.py
+++ src/kontor/__init__.py
@@ -1,2 +1,42 @@
+import argparse
+import logging
+from dataclasses import dataclass
+
+_logger = logging.getLogger()
+
+
 def main() -> None:
-    print("Hello from kontor!")
+    _setup_logging()
+
+    cli = _parse_cli()
+    if cli.debug:
+        _logger.setLevel(logging.DEBUG)
+    _logger.debug("cli=%r", cli)
+
+    _logger.info("Hello from kontor!")
+
+
+def _setup_logging() -> None:
+    logging.basicConfig(
+        format="%(levelname)s: %(message)s",
+        level=logging.INFO,
+    )
+
+
+def _parse_cli() -> _Cli:
+    parser = argparse.ArgumentParser(
+        description="Manages your home directory.",
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="print information helpful for debugging",
+    )
+
+    args = parser.parse_args()
+    return _Cli(**vars(args))
+
+
+@dataclass
+class _Cli:
+    debug: bool
